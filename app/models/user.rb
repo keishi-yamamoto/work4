7215f59b45b05d89c82dd2a02a67660a63b3b67e
vagrant <vagrant@localhost.localdomain>
2020-04-25 01:09:07 +0000

    [Finish]要件実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,32 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,:validatable
 
+  attachment :profile_image, destroy: false
+
   has_many :books, dependent: :destroy #userモデルの消去=>booksの消去
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  attachment :profile_image, destroy: false
-
-  validates :name, length: {maximum: 20, minimum: 2}
+  validates :name, presence: true
   validates :introduction, length: {maximum: 50}
-end
+
+  # フォロー取得。カラムごとにアソシエーションを行なっている？
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+end 
